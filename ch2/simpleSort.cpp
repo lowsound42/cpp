700131fb8015bd7e46a8406c5cc537126ea0e8b4
omario <o.khandxb@gmail.com>
2020-12-10 19:14:26 -0500

    like i said, wine time

--- ch2/simpleSort.cpp
+++ ch2/simpleSort.cpp
@@ -1,21 +1,22 @@
-#include "../std_lib_facilties.h"
+// #include "../std_lib_facilties.h"
 
-int main()
-{
-    int numbers[3];
-    int i,j,k;
-    int temp;
+// int main()
+// {
+//     int numbers[3];
+//     int i,j,k;
+//     int temp;
 
-    for (i = 0; i < 3; i++)
-    {
-        cout<<"Enter number " << i + 1 << " of 3: ";
-        cin>>numbers[i];   
-    }
+//     for (i = 0; i < 3; i++)
+//     {
+//         cout<<"Enter number " << i + 1 << " of 3: ";
+//         cin>>numbers[i];   
+//     }
 
-    for (i = 0; i<3; i++)
-    {
-        if (numbers[i+1] < numbers[i])
-    }
+//     for (i = 0; i<3; i++)
+//     {
+//         for (j=0)
+//         if (numbers[i+1] < numbers[i])
+//     }
 
 
-}
+// }
